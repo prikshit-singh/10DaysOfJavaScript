2b4851c68e4cee6e6fdb5c9a82c561b5493bbbf1
Prikshit <prikshitlatherlather@gmail.com>
2021-05-20 19:15:51 +0530

    day 4 classes

---
+++ class.js
@@ -0,0 +1,24 @@
+
+
+class Polygon{
+   constructor(arr){
+    this.arr=arr;
+   }
+
+    perimeter(){
+        var ans=0;
+        for(let i =0;i<this.arr.length;i++){
+            ans=ans+this.arr[i];
+        }
+        return ans;
+    }
+}
+
+//driver code
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
